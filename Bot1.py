96dbc9f5421cd23c21fbabd3f1ec1430d3805eed
DenizToptasKodland <164733668+DenizToptasKodland@users.noreply.github.com>
2024-05-04 13:27:10 +0300

    Create Bot1.py

---
+++ Bot1.py
@@ -0,0 +1,17 @@
+sync def mem(ctx):
+    with open('images/Jogoat.jpeg', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+    with open('images/Honored_One.jpeg', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+    with open('images/mewing.jpeg', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+    bot.run("")
